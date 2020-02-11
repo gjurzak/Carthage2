454d8f5a81367ae7ba03f2d3bfb5dbdf690b72e9
Greg Jurzak <greg.jurzak@mobile-5.com>
2020-02-11 18:37:43 +0100

    fsd

---
+++ Carthage/Checkouts/Carthage1/CarthageFramework/CarthageFramework/Sources/Greg.swift
@@ -0,0 +1,19 @@
+//
+//  Greg.swift
+//  CarthageFramework
+//
+//  Created by Grzegorz Jurzak on 11/02/2020.
+//  Copyright © 2020 HYD. All rights reserved.
+//
+
+import Foundation
+
+public struct Greg {
+
+    public init() { }
+
+    public func test(number: Int) -> Int {
+        return number + 1
+    }
+
+}
