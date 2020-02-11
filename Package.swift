755e048884f34b5928435c0b99ae1a3eec3fdf0e
Greg Jurzak <greg.jurzak@mobile-5.com>
2020-02-11 18:53:24 +0100

    no message

--- Package.swift
+++ Package.swift
@@ -4,25 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "CarthageFooo",
+    name: "CarthageFoo",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "CarthageFooo",
-            targets: ["CarthageFooo"]),
+            name: "CarthageFoo",
+            targets: ["CarthageFoo"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(
+             url: "https://github.com/gjurzak/Carthage1",
+             .branch("master")
+         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "CarthageFooo",
-            dependencies: []),
-        .testTarget(
-            name: "CarthageFoooTests",
-            dependencies: ["CarthageFooo"]),
+            name: "CarthageFoo",
+            dependencies: ["CarthageFramework"]),
     ]
 )
