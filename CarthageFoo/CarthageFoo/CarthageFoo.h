8ca533f1eb7bbdcf82e2f9573c6e3b6c67e811ef
Greg Jurzak <greg.jurzak@mobile-5.com>
2020-02-11 18:00:37 +0100

    init

---
+++ CarthageFoo/CarthageFoo/CarthageFoo.h
@@ -0,0 +1,19 @@
+//
+//  CarthageFoo.h
+//  CarthageFoo
+//
+//  Created by Grzegorz Jurzak on 11/02/2020.
+//  Copyright © 2020 HYD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CarthageFoo.
+FOUNDATION_EXPORT double CarthageFooVersionNumber;
+
+//! Project version string for CarthageFoo.
+FOUNDATION_EXPORT const unsigned char CarthageFooVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageFoo/PublicHeader.h>
+
+
